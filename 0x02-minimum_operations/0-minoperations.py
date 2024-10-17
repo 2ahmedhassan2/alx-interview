e111bcaf43c7c9ea5fe75382f5a44fa377c679f0
Ahmed Hassan <ah01157548281@mail.com>
2024-10-17 14:44:18 +0300

    s

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
 """
-Module to calculate the fewest number of operations needed
-to result in exactly n H characters in a file.
+Minimum operations to reach n characters
 """
-
 def minOperations(n):
+    """
+    Calculates the minimum operations to reach n characters
+    Args:
+        n (int): The number of characters to reach
+    Returns:
+        int: The minimum operations to reach n characters
+    """
+    # Edge cases [0, 1]
     if n <= 1:
         return 0
     operations = 0
     divisor = 2
     while n > 1:
-        while n % divisor == 0:
+        if n % divisor == 0:
+            n = n // divisor
             operations += divisor
-            n //= divisor
-        divisor += 1
+        else:
+            divisor += 1
     return operations
